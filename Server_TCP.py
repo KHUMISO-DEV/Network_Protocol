b8f44e59f868e739f89fcf815c68295ee03f7643
kagom <kagomotata5@gmail.com>
2026-03-03 00:12:16 +0200

    The server class created using the TCP as default

---
+++ Server_TCP.py
@@ -0,0 +1,42 @@
+import socket
+import threading
+
+class Server:
+    def __init__(self, host='127.0.0.1', port=5000):
+        self.host = host
+        self.port = port
+        self.clients = {}  # store {conn: ping_status}
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen()
+        print(f"[SERVER STARTED] Listening on {self.host}:{self.port}")
+
+    def broadcast(self, message, sender_conn):
+        for client_conn, ping in list(self.clients.items()):
+            if client_conn != sender_conn and ping:
+                try:
+                    client_conn.send(message.encode('utf-8'))
+                except:
+                    self.clients[client_conn] = False  # mark inactive
+
+    def handle_client(self, conn, addr):
+        print(f"[NEW CONNECTION] {addr} connected.")
+        self.clients[conn] = True  # mark as active
+        while self.clients[conn]:
+            try:
+                msg = conn.recv(1024).decode('utf-8')
+                if not msg:
+                    break
+                print(f"[{addr}] {msg}")
+                self.broadcast(msg, conn)
+            except:
+                break
+        conn.close()
+        self.clients[conn] = False
+        print(f"[DISCONNECTED] {addr}")
+
+    def run(self):
+        while True:
+            conn, addr = self.server_socket.accept()
+            thread = threading.Thread(target=self.handle_client, args=(conn, addr))
+            thread.start()
